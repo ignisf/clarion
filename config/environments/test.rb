867d2f4faba44d3bc3da5c28629af617d09a82f3
Petko Bordjukov <bordjukov@gmail.com>
2014-08-07 14:06:34 +0300

    Specify default ActionMailer host for dev and test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Use localhost for default host
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
