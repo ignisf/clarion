6728b3d059877f2042572f0878679ad05dea6d72
Petko Bordjukov <bordjukov@gmail.com>
2014-08-30 17:31:29 +0300

    Lecture creation

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,10 +1,19 @@
 class LecturesController < ApplicationController
+  before_filter :authenticate_user!
+  before_action :assign_lecture, only: [:show]
+
   def new
     @lecture = Lecture.new
   end
 
   def create
-    head :created
+    @lecture = current_user.lectures.build lecture_params
+
+    if @lecture.save
+      redirect_to @lecture
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -14,5 +23,15 @@ class LecturesController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def assign_lecture
+    @lecture = Lecture.find params[:id]
+  end
+
+  def lecture_params
+    params.require(:lecture).permit [:title, :subtitle, :length, :language, :abstract, :description, :notes, :track_id]
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ActiveRecord::Base
   validates :length, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :abstract, presence: true
   validates :description, presence: true
+  validates :track, inclusion: { in: (Conference.current.try(:tracks) || []) }
 
   belongs_to :track
   has_one :conference, through: :track
