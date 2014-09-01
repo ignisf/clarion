6361512fe4ff93c1dc6156f52dee03f5f8cf0a2f
Petko Bordjukov <bordjukov@gmail.com>
2014-09-01 12:32:03 +0300

    Flip on locale setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options(options={})
+    {locale: I18n.locale}
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   protected
 
