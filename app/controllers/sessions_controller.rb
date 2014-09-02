b0aa271b7b666133e5a144182dcac579b29edf99
Petko Bordjukov <bordjukov@gmail.com>
2014-09-02 12:35:00 +0300

    Display a flash after sign in of user without SpeakerProfile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < Devise::SessionsController
     if user.speaker_profile.present?
       stored_location_for(user) || signed_in_root_path(user)
     else
+      flash[:alert] = I18n.t(:please_fill_in_your_speaker_profile)
       edit_user_registration_path
     end
   end
