c93900702fb8b539f906f1874e9dbee87befcc39
Petko Bordjukov <bordjukov@gmail.com>
2014-09-06 17:17:09 +0300

    Send event notifications to the core team

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 class EventMailer < ActionMailer::Base
   def new_event_notification(event)
-    @users = User.where(admin: true)
     @event = event
-    mail(to: @users.map(&:email), subject: "Ново предложение за #{@event.class.model_name.human.mb_chars.downcase.to_s}: #{@event.title}")
+    mail(to: 'core@openfest.org', subject: "Ново предложение за #{@event.class.model_name.human.mb_chars.downcase.to_s}: #{@event.title}")
   end
 end
