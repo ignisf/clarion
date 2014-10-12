27677e2d0a7908a0d3f9919c62ed04716edfe1eb
Petko Bordjukov <bordjukov@gmail.com>
2014-10-12 13:31:48 +0300

    Add a square version of the photos for the shedule

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -39,6 +39,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [50, 50]
   end
 
+  version :schedule do
+    process :resize_to_fill => [100, 100]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
