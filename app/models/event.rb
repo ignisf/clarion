408457a76591157cf6d181f5af83cdd4e711ba17
Petko Bordjukov <bordjukov@gmail.com>
2015-04-17 02:02:17 +0300

    Remove unneeded functionality from Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,6 @@ class Event < ActiveRecord::Base
   validates :length, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :abstract, presence: true
   validates :description, presence: true
-  validates :track_id, inclusion: { in: (Conference.current.try(:tracks) || []).map(&:id) }
   validates :agreement, acceptance: true
 
   belongs_to :track
@@ -11,8 +10,6 @@ class Event < ActiveRecord::Base
   belongs_to :user # XXX: Transition to candidate_speaker
   belongs_to :candidate_speaker, class_name: 'User', foreign_key: 'user_id'
   has_and_belongs_to_many :speakers, class_name: 'SpeakerProfile'
-
-  after_create :send_new_event_notification
 
   enum state: [:undecided, :approved, :rejected, :backup]
 
@@ -22,18 +19,13 @@ class Event < ActiveRecord::Base
   STATE_TO_GLYPH = {undecided: 'question-sign', rejected: 'remove', approved: 'ok', backup: 'retweet'}
   STATE_TO_CLASS = {undecided: 'warning', rejected: 'danger', approved: 'success', backup: 'info'}
 
-  def send_acceptance_notification!
-    EventMailer.acceptance_notification(self).deliver
-    touch :acceptance_notification_sent_at
-  end
-
   def confirmed?
     !!confirmed_at
   end
 
   private
 
-  def send_new_event_notification
-    EventMailer.new_event_notification(self).deliver
+  def self.inheritance_column
+    'something'
   end
 end
