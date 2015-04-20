d6e3f9ba5da6dd75d4620be900b773969a65ff62
Petko Bordjukov <bordjukov@gmail.com>
2015-04-20 18:34:17 +0300

    Make the status attribute of Proposition an enum

--- app/models/proposition.rb
+++ app/models/proposition.rb
@@ -2,4 +2,6 @@ class Proposition < ActiveRecord::Base
   belongs_to :proposer, class_name: 'User'
   belongs_to :proposition_accepting, polymorphic: true
   belongs_to :proposable, polymorphic: true
+
+  enum status: [:undecided, :approved, :rejected, :backup]
 end
