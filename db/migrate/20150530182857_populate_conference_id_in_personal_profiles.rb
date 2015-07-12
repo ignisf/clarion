08bbd1ffa1f1110f3741081fc4753f3e76cebb12
Andrew Radev <andrey.radev@gmail.com>
2015-07-12 11:22:19 +0300

    Fix migration that fails in build

--- db/migrate/20150530182857_populate_conference_id_in_personal_profiles.rb
+++ db/migrate/20150530182857_populate_conference_id_in_personal_profiles.rb
@@ -6,7 +6,10 @@ end
 
 class PopulateConferenceIdInPersonalProfiles < ActiveRecord::Migration
   def change
-    conference = Conference.first!
-    PersonalProfile.where(conference_id: nil).update_all(conference_id: conference.id)
+    conference = Conference.first
+
+    if conference
+      PersonalProfile.where(conference_id: nil).update_all(conference_id: conference.id)
+    end
   end
 end
