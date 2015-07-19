2fe1fd2f098d180b2028eb4f69e3d1c95d54e6f2
Petko Bordjukov <bordjukov@gmail.com>
2015-07-19 21:08:29 +0300

    OpenFest: Implement WelcomeController#index view

--- lib/open_fest/app/controllers/open_fest/application_controller.rb
+++ lib/open_fest/app/controllers/open_fest/application_controller.rb
@@ -1,4 +1,12 @@
 module OpenFest
   class ApplicationController < ActionController::Base
+    before_action :set_current_conference
+
+    private
+
+    #TODO: Make this display a nice 404 message when the conference is not found
+    def set_current_conference
+      @current_conference ||= Conference.find(1)
+    end
   end
 end
