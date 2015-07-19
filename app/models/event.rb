d381b800c4e9cbb6bf0042ad04387947c93d39a7
Petko Bordjukov <bordjukov@gmail.com>
2015-07-19 14:32:25 +0300

    Add associations to Track and Conference to Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ActiveRecord::Base
   include Proposable
 
+  has_one :track, through: :proposition, source: :proposition_accepting, source_type: Track
+  has_one :conference, through: :track
+
   validates :title, presence: true
   validates :length, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :abstract, presence: true
