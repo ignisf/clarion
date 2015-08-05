48119a687ce278cef49d7b021ca75c0720f2ab0c
Petko Bordjukov <bordjukov@gmail.com>
2015-08-05 16:06:35 +0300

    OpenFest: Fix form wrapper and paths in Devise views

--- lib/open_fest/app/controllers/open_fest/users/passwords_controller.rb
+++ lib/open_fest/app/controllers/open_fest/users/passwords_controller.rb
@@ -1,3 +1,7 @@
 class OpenFest::Users::PasswordsController < Devise::PasswordsController
   include OpenFest::Users::DeviseController
+
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    new_user_session_path
+  end
 end
