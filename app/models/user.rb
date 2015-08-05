adb5ca34b606bd3fb6e1ba945d24d519c88182e7
Andrew Radev <andrey.radev@gmail.com>
2015-08-05 15:07:07 +0300

    Remove half-baked methods
    We'll think of something better later on.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,38 +12,7 @@ class User < ActiveRecord::Base
 
   default_scope { order id: :desc }
 
-  def duplicate_last_personal_profile(conference)
-    if personal_profiles.any?
-      new_personal_profile = personal_profiles.last.dup
-      new_personal_profile.conference = conference
-      new_personal_profile
-    end
-  end
-
-  def find_or_initialize_personal_profile(conference)
-    if personal_profile(conference).present?
-      personal_profile(conference)
-    elsif personal_profiles.any?
-      duplicate_last_personal_profile(conference)
-    else
-      personal_profiles.build(conference: conference)
-    end
-  end
-
   def toggle_admin!
     update admin: !admin
-  end
-
-  def clone_recent_profile(new_conference)
-    recent_profile = personal_profiles.order('created_at DESC').first
-
-    personal_profiles.build(conference: new_conference) do |new_profile|
-      if recent_profile.present?
-        new_profile.attributes = recent_profile.attributes.except(
-          "id", "created_at", "updated_at", "conference_id"
-        )
-        new_profile.remote_picture_url = recent_profile.picture.url
-      end
-    end
   end
 end
