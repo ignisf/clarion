a27fd44ad04de8337d20c05516ac6c73852473e8
Petko Bordjukov <bordjukov@gmail.com>
2015-08-10 20:47:32 +0300

    Fix foreign key constraint for Participations

--- db/migrate/20150729135346_create_participations.rb
+++ db/migrate/20150729135346_create_participations.rb
@@ -1,11 +1,13 @@
 class CreateParticipations < ActiveRecord::Migration
   def change
     create_table :participations do |t|
-      t.references :participant, index: true, foreign_key: true
+      t.references :participant, index: true
       t.references :event, index: true, foreign_key: true
       t.boolean :approved, default: false
 
       t.timestamps null: false
     end
+
+    add_foreign_key :participations, :users, column: :participant_id
   end
 end
