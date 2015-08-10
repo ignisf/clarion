dd14ca650d73d623c424d1d797ee02974711a1fc
Andrew Radev <andrey.radev@gmail.com>
2015-08-10 15:35:32 +0300

    Implement changing the language

--- lib/open_fest/app/controllers/open_fest/application_controller.rb
+++ lib/open_fest/app/controllers/open_fest/application_controller.rb
@@ -3,5 +3,16 @@ module OpenFest
     include ::CurrentConferenceAssigning
 
     before_filter :require_current_conference!
+    before_filter :set_locale
+
+    def set_locale
+      if params[:locale]
+        I18n.locale = params[:locale]
+      end
+    end
+
+    def default_url_options(options = {})
+      options.merge(locale: params[:locale])
+    end
   end
 end
--- lib/open_fest/config/routes.rb
+++ lib/open_fest/config/routes.rb
@@ -1,8 +1,10 @@
 OpenFest::Engine.routes.draw do
   root to: 'events#index'
 
-  devise_for :users, module: 'open_fest/users'
+  scope '(/:locale)' do
+    devise_for :users, module: 'open_fest/users'
 
-  resource :personal_profile, path: 'profile'
-  resources :events
+    resource :personal_profile, path: 'profile'
+    resources :events
+  end
 end
