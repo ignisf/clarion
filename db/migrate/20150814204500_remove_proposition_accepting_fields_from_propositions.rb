b2dd7bc0a899fce1d36768c3361a0db7201c01e0
Petko Bordjukov <bordjukov@gmail.com>
2015-08-15 04:36:40 +0300

    The proposition_accepting columns did not have a FK constraint

--- db/migrate/20150814204500_remove_proposition_accepting_fields_from_propositions.rb
+++ db/migrate/20150814204500_remove_proposition_accepting_fields_from_propositions.rb
@@ -1,5 +1,5 @@
 class RemovePropositionAcceptingFieldsFromPropositions < ActiveRecord::Migration
   def change
-    remove_reference :propositions, :proposition_accepting, index: true, foreign_key: true, polymorphic: true
+    remove_reference :propositions, :proposition_accepting, index: true, polymorphic: true
   end
 end
