1b07344efbfca0d813f5efa05d88fa606ddd96b0
Petko Bordjukov <bordjukov@gmail.com>
2015-08-20 01:45:57 +0300

    Actually send an email when a proposition is created

--- app/models/proposition.rb
+++ app/models/proposition.rb
@@ -1,10 +1,10 @@
 class Proposition < ActiveRecord::Base
   belongs_to :proposer, class_name: 'User'
   belongs_to :proposable, polymorphic: true
-
   enum status: [:undecided, :approved, :rejected, :backup]
-
   delegate :proposable_title, :proposable_type, :proposable_description, to: :proposable
+
+  after_create :send_creation_notification
 
   def send_creation_notification
     PropositionMailer.new_proposition_notification(self).deliver_later
