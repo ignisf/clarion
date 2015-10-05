901a15f06497858d714f37e7a0591853b6aa5349
Petko Bordjukov <bordjukov@gmail.com>
2015-10-05 15:29:22 +0300

    Export info for the event type, too

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,9 +1,9 @@
 class Api::EventsController < Api::ApplicationController
   def index
     @conference = find_conference
-    @events = @conference.events.includes(:track)
+    @events = @conference.events.includes(:track, :event_type)
 
-    render json: @events, include: :track
+    render json: @events, include: [:track, :event_type]
   end
 
   private
