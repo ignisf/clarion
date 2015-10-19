d7af47c0c684378daa4885305d8d881ce628417b
Petko Bordjukov <bordjukov@gmail.com>
2015-10-19 12:37:56 +0300

    Add a mailer for event confirmations

---
+++ app/mailers/event_mailer.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+class EventMailer < ActionMailer::Base
+  def confirmation_request(event)
+    @event = event
+    I18n.locale = @event.proposer.language
+
+    mail to: @event.proposer.email,
+         from: 'program@openfest.org',
+         subject: I18n.t('event_mailer.acceptance_notification.subject',
+                         conference: @event.conference.title,
+                         submission_type: @event.event_type.name.mb_chars.downcase.to_s,
+                         title: @event.title)
+  end
+end
