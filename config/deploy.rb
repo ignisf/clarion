c523a80d66de09cc48c03b1675c0087e16559a96
Petko Bordjukov <bordjukov@gmail.com>
2015-11-05 13:17:59 +0200

    Preserve picture uploads on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/home/barf/clarion'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/uploads', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
