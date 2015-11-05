61a3d06667f56d4635342c2838a12218c1589b7b
Petko Bordjukov <bordjukov@gmail.com>
2015-11-05 13:09:13 +0200

    Add a rejection notification

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -11,4 +11,17 @@ class EventMailer < ActionMailer::Base
                          submission_type: @event.event_type.name.mb_chars.downcase.to_s,
                          title: @event.title)
   end
+
+  def rejection_notification(event)
+    @event = event
+    I18n.locale = @event.proposer.language
+
+    mail to: @event.proposer.email,
+         from: 'program@openfest.org',
+         subject: I18n.t('event_mailer.rejection_notification.subject',
+                         conference: @event.conference.title,
+                         submission_type: @event.event_type.name.mb_chars.downcase.to_s,
+                         title: @event.title)
+  end
+
 end
