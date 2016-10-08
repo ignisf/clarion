8a0ae821652eb7a159832df2ff580f410b0f9ba7
Petko Bordjukov <bordjukov@gmail.com>
2016-10-09 02:55:31 +0300

    Reintroduce the EventsHelper::participants_names_or_emails method

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -13,4 +13,12 @@ module EventsHelper
       end
     end.join(', ').html_safe
   end
+
+  def participant_names_or_emails(event)
+    event.participants.map do |participant|
+      participant.try(:personal_profile, current_conference).try(:name) ||
+        participant.personal_profiles.last.try(:name) ||
+        participant.email
+    end
+  end
 end
