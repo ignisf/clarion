921bde2d4e7fbf335dec5f1c54bee80c1bea7143
Petko Bordjukov <bordjukov@gmail.com>
2016-10-08 21:19:58 +0300

    Turn on event sorting

--- app/models/event_search.rb
+++ app/models/event_search.rb
@@ -1,9 +1,11 @@
 class EventSearch
-  include SearchObject.module
+  include SearchObject.module(:sorting)
 
   option(:title)         { |scope, value| scope.where title: value }
   option(:language)      { |scope, value| scope.where language: value }
   option(:track_id)      { |scope, value| scope.where track_id: value }
   option(:event_type_id) { |scope, value| scope.where event_type_id: value }
   option(:status)        { |scope, value| scope.joins(:proposition).where(propositions: {status: value}) }
+
+  sort_by :title
 end
