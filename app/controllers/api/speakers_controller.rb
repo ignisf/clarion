4d93de0e2165cb9b3b7bdd1ae3a0c88f052a411a
Petko Bordjukov <bordjukov@gmail.com>
2016-10-24 18:51:16 +0300

    Limit the personal profiles to the current conference

--- app/controllers/api/speakers_controller.rb
+++ app/controllers/api/speakers_controller.rb
@@ -3,6 +3,6 @@ class Api::SpeakersController < Api::ApplicationController
   before_filter :require_current_conference!
 
   def index
-    @speakers = PersonalProfile.joins(user: {participations: {event: :proposition}}).where(events: {id: current_conference.approved_events.pluck(:id)}).distinct
+    @speakers = PersonalProfile.joins(user: {participations: {event: :proposition}}).where(events: {id: current_conference.approved_events.pluck(:id)}, conference: current_conference).distinct
   end
 end
