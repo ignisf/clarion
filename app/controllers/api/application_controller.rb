b732aff523b3d274526b828b684f802cc144b544
Vencislav Atanasov <user890104@users.noreply.github.com>
2016-11-04 15:34:44 +0200

    Add CORS header in order to access the public API from JavaScript
    (I have no idea what I'm doing)

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,2 +1,7 @@
 class Api::ApplicationController < ::ApplicationController
+  def set_access_control_headers
+    if request.format.json?
+      response.headers['Access-Control-Allow-Origin'] = '*'
+    end
+  end
 end
