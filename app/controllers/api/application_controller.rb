b5192a89a9027fad5e8aebcb4fb3e7ba58c6d136
Vencislav Atanasov <user890104@users.noreply.github.com>
2016-11-17 18:46:40 +0200

    Move method to a more appropriate place

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,7 +1,2 @@
 class Api::ApplicationController < ::ApplicationController
-  def set_access_control_headers
-    if request.format.json?
-      response.headers['Access-Control-Allow-Origin'] = '*'
-    end
-  end
 end
--- app/controllers/concerns/public_api_exposing.rb
+++ app/controllers/concerns/public_api_exposing.rb
@@ -3,6 +3,12 @@ require 'active_support/concern'
 module PublicApiExposing
   extend ActiveSupport::Concern
 
+  def set_access_control_headers
+    if request.format.json?
+      response.headers['Access-Control-Allow-Origin'] = '*'
+    end
+  end
+  
   included do
     before_action :set_access_control_headers
   end
