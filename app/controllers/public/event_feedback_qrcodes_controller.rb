49b85190f2908e2ac44630b2dde748f0bd0edc66
Petko Bordjukov <bordjukov@gmail.com>
2019-10-12 20:56:16 +0300

    Add feedback link QR code to confirmation emails

--- app/controllers/public/event_feedback_qrcodes_controller.rb
+++ app/controllers/public/event_feedback_qrcodes_controller.rb
@@ -1,11 +1,10 @@
 class Public::EventFeedbackQrcodesController < Public::ApplicationController
   def show
     event = current_conference.events.joins(:proposition).approved.find(params[:event_id])
-    @qr = RQRCode::QRCode.new(new_event_feedback_url(event_id: event.id), level: :l)
 
     respond_to do |format|
       format.svg do
-        render(inline: @qr.as_svg(shape_rendering: "crispEdges", module_size: 11, fill: "ffffff", offset: 10),
+        render(inline: helpers.feedback_qr_code_as_svg(event.id),
                filename: "feedback_qr_code_#{event.id}.svg")
       end
     end
--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -2,7 +2,10 @@ class EventMailer < ActionMailer::Base
   def confirmation_request(event)
     @event = event
     I18n.locale = @event.proposer.language
-
+    attachments['feedback-link-qr-code.svg'] = {
+      mime_type: 'image/svg+xml',
+      content: helpers.feedback_qr_code_as_svg(@event.id)
+    }
     mail to: @event.proposer.email,
          from: "program@openfest.org",
          subject: I18n.t("event_mailer.acceptance_notification.subject",
