05617add98f023a492bec4786a1ff5f974781b1f
Petko Bordjukov <bordjukov@gmail.com>
2022-10-19 00:57:37 +0300

    Remove eagerness

--- app/controllers/management/feedback_controller.rb
+++ app/controllers/management/feedback_controller.rb
@@ -7,7 +7,7 @@ module Management
     private
 
     def find_conference
-      Conference.eager_load(:feedbacks, :feedbacks_with_comment, :event_feedbacks, :event_feedbacks_with_comment).find(params[:conference_id])
+      Conference.find(params[:conference_id])
     end
   end
 end
