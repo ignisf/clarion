dcf1f3ff2e787a1c64231c57fdbfb2a2a11bc725
Petko Bordjukov <bordjukov@gmail.com>
2024-02-19 22:06:16 +0200

    Rails 6.1.7.6 Upgrade

--- config/application.rb
+++ config/application.rb
@@ -11,28 +11,26 @@ module Clarion
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
     config.time_zone = "Sofia"
+    # config.eager_load_paths << Rails.root.join("extras")
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:bg, :en]
     config.i18n.default_locale = :bg
 
     config.active_record.schema_format = :sql
 
     config.generators do |g|
-      g.test_framework :rspec, fixtures: true,
-                               view_specs: false,
-                               helper_specs: false,
-                               routing_specs: false,
-                               request_specs: false
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
 
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
